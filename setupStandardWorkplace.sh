7e8f3636485b9d9e01acebb615ad63ff3ce4aaa9
David Zellhöfer <elektrobohemian@users.noreply.github.com>
2026-09-05 11:58:56 +0200

    Setup standard workplace with directories and tools
    The script sets up a standard workplace by creating directories, installing tools via Homebrew, configuring the shell with Starship, and adding a macOS standard .gitignore.

---
+++ setupStandardWorkplace.sh
@@ -0,0 +1,31 @@
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+printf "\n${YELLOW}Creating typical directories below home${NC}\n"
+
+
+mkdir -p ~/temp
+mkdir -p ~/src
+
+printf "\n${YELLOW}Setting up typical tools (requires Homebrew)${NC}\n"
+brew install starship
+brew install duf
+brew install htop
+brew install nvtop
+brew install ffmpeg
+brew install tldr
+brew install uv
+brew install fastfetch
+brew install yt-dlp
+brew install --cask meld
+brew install coreutils
+brew install uninstallpkg
+
+printf "\n${YELLOW}Shell configuration (requires starship)${NC}\n"
+
+printf '\n#daz: starship.rs\nstarship init fish | source' >> ~/.config/fish/config.fish
+curl --output ~/.config/starship.toml https://raw.githubusercontent.com/elektrobohemian/linux_utilities/refs/heads/main/starship.toml
+
+printf "\n${YELLOW}Adding macOS standard .gitignore${NC}\n"
+mkdir -p ~/.config/git
+curl --output ~/.config/git/ignore https://raw.githubusercontent.com/elektrobohemian/linux_utilities/refs/heads/main/gitignore_macos
